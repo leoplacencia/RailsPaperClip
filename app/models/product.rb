29dafe91d956eee311d4287112ab947c9b9d5676
Leonardo Placencia <placencia.leo@gmail.com>
2018-03-28 12:03:09 -0300

    Corregidos algunos errores de formulario y validacion de imagen

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ActiveRecord::Base
     has_attached_file :image, styles: { medium: '200x200>', thumb: '48x48>' }
+    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   end
